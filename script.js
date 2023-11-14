b25437a0309dc72b81e886411a17aa1992424e29
LucaCaligola <luca.caligola@gmail.com>
2023-11-14 15:39:45 +0100

    First milestone completed

---
+++ script.js
@@ -0,0 +1,31 @@
+//  Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+//  per i multipli di 3 stampi “Fizz” al posto del numero
+//  per i multipli di 5 stampi “Buzz” al posto del numero
+//  per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+
+
+for (let i = 1; i < 101; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log ('FizzBuzz')
+    }
+
+    else if (i % 3 == 0) {
+        console.log ('Fizz')
+    }
+
+    else if (i % 5 == 0) {
+        console.log ('Buzz')
+    }
+
+    else if (i % 3 == 0 && i % 5 == 0) {
+        console.log ('FizzBuzz')
+    }
+
+    else {
+        console.log (i)
+    }
+    
+}
+    
